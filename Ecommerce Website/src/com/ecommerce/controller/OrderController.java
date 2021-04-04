37d5456a9275d2f4c8fde67b3b14df74c5169622
TranMinn <1801040141@s.hanu.edu.vn>
2021-04-04 10:45:08 +0700

    hmmmmm

--- Ecommerce Website/src/com/ecommerce/controller/OrderController.java
+++ Ecommerce Website/src/com/ecommerce/controller/OrderController.java
@@ -1,5 +1,75 @@
 package com.ecommerce.controller;
 
-public class OrderController {
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.ecommerce.model.Cart;
+import com.ecommerce.model.CartItem;
+import com.ecommerce.model.User;
+import com.ecommerce.service.CartItemService;
+import com.ecommerce.service.CartService;
+import com.ecommerce.service.UserService;
+import com.ecommerce.service.impl.CartServiceImpl;
+import com.ecommerce.service.impl.CartServiceItemImpl;
+import com.ecommerce.service.impl.UserServiceImpl;
+import com.ecommerce.tools.SendMail;
+import com.ecommerce.util.RandomUUID;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = "/member/order")
+public class OrderController extends HttpServlet implements Serializable {
+	UserService userService = new UserServiceImpl();
+	CartService cartService = new CartServiceImpl();
+	CartItemService cartItemService = new CartServiceItemImpl();
+	long time = System.currentTimeMillis();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/view/client/view/check-out.jsp");
+		dispatcher.forward(req, resp);
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		Object obj = session.getAttribute("account");
+		User buyer = (User) obj;
+		Cart cart = new Cart();
+		cart.setBuyer(buyer);
+		cart.setBuyDate(new java.sql.Date(time));
+		cart.setId(RandomUUID.getRandomID());
+		cartService.insert(cart);
+
+		Object objCart = session.getAttribute("cart");
+		if (objCart != null) {
+			// ep ve dung kieu cua no khi them vao o phan them vao gio hang controller
+			@SuppressWarnings("unchecked")
+			Map<Integer, CartItem> map = (Map<Integer, CartItem>) objCart;
+
+			for (CartItem cartItem : map.values()) {
+				cartItem.setCart(cart);
+				cartItem.setId(RandomUUID.getRandomID());
+				SendMail sm = new SendMail();
+				sm.sendMail(cart.getBuyer().getEmail(), "ASHION", " Email! ");
+				cartItemService.insert(cartItem);
+				
+			}
+
+		}
+		session.removeAttribute("cart");
+		resp.sendRedirect(req.getContextPath() + "/waiting");
+	}
 
 }
